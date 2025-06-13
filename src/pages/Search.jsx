dc0f754d6aecd1e457253ddb22b892209807c508
Adrian Camacho <acr77frb@gmail.com>
2025-06-13 15:23:36 -0600

    Implemented dynamic tab rendering.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -5,19 +5,17 @@ import "../styles/pages/Search.css"
 import { isObjEmpty } from "../modules/checkers"
 import { Container } from '@mui/material';
 import ResourceTabs from '../components/ResourceTabs';
+import NotFoundScreen from "../components/NotFoundScreen";
+import Spinner from "../components/Spinner";
  const Search = () => {
   const {word} = useParams();
   const [wordData, setWordData] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    setWordData({});
-    setLoading(true);
-    setError(null);
-
-    fetchWordData()
-  }, [word]);
+  const resourcesAvailable = arr => {
+    return arr.some(e => e.ok);
+  };
   const fetchWordData = async () => {
     try {
       const responseDLE = await fetch(
@@ -30,15 +28,26 @@ import ResourceTabs from '../components/ResourceTabs';
         `${process.env.REACT_APP_BASE_URL}gtg/${encodeURIComponent(word)}`
       );
 
-      /* if (!responseDLE.ok && !responseDPD.ok && !responseGTG.ok) {
-        throw new Error("¡Ups! Tuvimos un problema. Vuelve a intentarlo más tarde.");
-      } */
-
       if (resourcesAvailable([responseDLE, responseDPD, responseGTG])) {
         const wordDataDLE = responseDLE.ok? await responseDLE.json() : {};
         const wordDataDPD = responseDPD.ok? await responseDPD.json() : {};
         const wordDataGTG = responseGTG.ok? await responseGTG.json() : {};
-        setWordData({DLE: wordDataDLE, DPD: wordDataDPD, GTG: wordDataGTG});
+
+        const newWordData = {
+          DLE: {
+            name: "Diccionario de la Lengua Española (DLE)",
+            ...wordDataDLE
+          },
+          DPD: {
+            name: "Diccionario Panhispánico de Dudas (DPD)",
+            ...wordDataDPD
+          },
+          GTG: {
+            name: "Glosario de Términos Gramaticales (GTG)",
+            ...wordDataGTG
+          }
+        }
+        setWordData(newWordData);
       }
     } catch (err) {
       setError(err.message);
@@ -46,24 +55,32 @@ import ResourceTabs from '../components/ResourceTabs';
       setLoading(false);
     }
   };
-  const resourcesAvailable = arr => {
-    return arr.some(e => e.ok);
-  }
+
+  useEffect(() => {
+    setWordData({});
+    setLoading(true);
+    setError(null);
+    fetchWordData()
+  }, [word]);
 
   return (
-    <>
+    <main className="search-main">
       <Navbar></Navbar>
+      {loading && <Spinner/>}
       {
-
-        <main id="search-main">
+        !isObjEmpty(wordData)?
+        <div className="search-container">
           <h1 id="term">{word}</h1>
-          {<Container id="resources">
+          {
+          <Container id="resources">
             <ResourceTabs wordData={wordData}/>
-          </Container>}
-          {/* <div>{JSON.stringify(wordData)}</div> */}
-        </main>
+          </Container>
+          }
+    </div>
+        :
+        !loading && <NotFoundScreen type="search" word={word}/>
       }
-    </>
+    </main>
   )
 }
 
